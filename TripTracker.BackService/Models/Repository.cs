8ead440c1c943d5b217c72998ea9409f883abe15
ashutoshpith <amxdp18@gmail.com>
2019-02-14 21:10:22 +0530

    model repository is added

--- TripTracker.BackService/Models/Repository.cs
+++ TripTracker.BackService/Models/Repository.cs
@@ -7,6 +7,43 @@ namespace TripTracker.BackService.Models
 {
     public class Repository
     {
+        private  List<Trip> MyTrips = new List<Trip>{
+            new Trip{
+                    Id = 1,
+                    Name = "ashutosh",
+                    StartDate = new DateTime(2018, 5, 9),
+                    EndDate = new DateTime(2018,5, 9)
+                     },
+            new Trip{
+                    Id = 2,
+                    Name = "Bhavana",
+                    StartDate = new DateTime(2019,2,17),
+                    EndDate = new DateTime(2019,5,7)
+                     },
+            new Trip{
+                    Id = 3,
+                    Name = "Unknown",
+                    StartDate = new DateTime(2019,2,17),
+                    EndDate = new DateTime(2019,5,7)
+                     }
 
+    };
+
+       
+        public void Add(Trip newTrip) { 
+            MyTrips.Add(newTrip);
+ 
+                              }
+        public void Update(Trip tripToUpdate) {
+            MyTrips.Remove(MyTrips.First(t => t.Id == tripToUpdate.Id));
+            Add(tripToUpdate);
+                                              }
+
+        public void Remove(int Id){
+            // MyTrips.Remove(MyTrips(t => t.Id == Id));
+                                  }
+
+
+
+  }
     }
-}
